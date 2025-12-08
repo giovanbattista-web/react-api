b07faa7187c8cf15a9edb8e2b000617f36397a5d
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-12-08 07:35:44 +0100

    [UPD] preparing cards for each actress by showing some informations

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,24 +6,52 @@ import axios from 'axios';
 const actressEndpoint = 'https://lanciweb.github.io/demo/api/actresses/'
 const Main = () => {
     // definiamo la variabile di stato per gli attori/attrici
-    const [actress, setActress] = useState([]);
+    const [actresses, setActresses] = useState([]);
 
     // definizione della funzione che effettua la chiamata ajax
     const getActress = () => {
         axios.get(actressEndpoint).then((resp) => {
-            console.log(resp.data);
+            // console.log(resp.data);
+            setActresses(resp.data)
         });
     };
 
-    useEffect(() => getActress(), [])
+    useEffect(() => getActress(), []);
 
     return (
         <div className='container'>
-            <div className='row'>
-                <div className='col-12'>Hello World</div>
+            <div className='row mt-4'>
+                <div className='col-12'>
+                    <h2>Attrici</h2>
+                </div>
+            </div>
+            <div className='row g-3'>
+                {actresses.map((actress) => {
+                    return (
+                        <>
+                            <div className='col-12 col-md-6 col-lg-3'>
+                                <div className='bg-black text-white'>
+                                    <div className='card-image'>
+                                        <img src={actress.image} className='img-fluid' />
+                                    </div>
+                                    <div className='card-info'>
+                                        <h3>{actress.name}</h3>
+                                        <p>
+                                            <em>Birth Year: {actress.birth_year}</em>
+                                            <br />
+                                            Nationality: {actress.nationality}
+                                        </p>
+                                        <p>{actress.biography}</p>
+                                        <p>{actress.awards}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    )
+                })}
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Main;
