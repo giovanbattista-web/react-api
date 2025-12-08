f545046a59026bd39f57f1abd2119a5fb6bc781f
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-12-08 06:58:37 +0100

    [UPD] retrieve the list of actresses and print them on the console

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,22 @@
+import { Axios } from 'axios';
+import { useState, useEffect } from 'react';
 import React from 'react';
+import axios from 'axios';
 
+const actressEndpoint = 'https://lanciweb.github.io/demo/api/actresses/'
 const Main = () => {
+    // definiamo la variabile di stato per gli attori/attrici
+    const [actress, setActress] = useState([]);
+
+    // definizione della funzione che effettua la chiamata ajax
+    const getActress = () => {
+        axios.get(actressEndpoint).then((resp) => {
+            console.log(resp.data);
+        });
+    };
+
+    useEffect(() => getActress(), [])
+
     return (
         <div className='container'>
             <div className='row'>
